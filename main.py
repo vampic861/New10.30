f50b19cece3fb9f2d19813b003e69fbeae5f5538
vampic861 <125185219+vampic861@users.noreply.github.com>
2024-10-30 16:41:00 +1000

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+#1st program
+print(9 ** 0.5 * 5)
+#2nd program
+print(9.99 > 9.98 and 1000 != 1001)
+#3rd program
+print(2 * 2 + 2, 2 * (2 + 2))
+print(2 * 2 + 2 == 2 * (2 + 2))
+#3rd program
+print(int(float('123.456') * 10) % 10)
